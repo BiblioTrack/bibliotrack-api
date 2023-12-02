1ab295ffeecee4929d153020830ae0e406175f55
stasbern1 <stasbern@yahoo.com>
2023-12-02 19:38:58 +0100

    books and book routing

--- routes/cors.js
+++ routes/cors.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const whitelist = ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:8080', 'https://lib-manage.herokuapp.com/','https://lib-manage.herokuapp.com:3000','https://lib-manage.herokuapp.com:5000'];
+const whitelist = ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:8080', 'https://bibliotrack.herokuapp.com/','https://bibliotrack.herokuapp.com:3000','https://bibliotrack.herokuapp.com:5000'];
 var corsOptionsDelegate = (req, callback) => {
     var corsOptions;
     console.log(req.header('Origin'));
