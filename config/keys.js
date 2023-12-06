dbd4411a805ef7efc6b31a09bba4ba581315aa74
mutabay <tyyp-mstf@hotmail.com>
2023-12-06 22:04:12 +0100

    Database connection string has changed

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports={
-    mongoURI: `mongodb://localhost:27017/bibliotrack`,
-    //mongoURI: 'mongodb+srv://db_username:lms2434@lms.jyjs2.mongodb.net/libmanagement',
+    //mongoURI: `mongodb://localhost:27017/bibliotrack`,
+    mongoURI: 'mongodb+srv://bibliotrack:admin@ase23.ecc730x.mongodb.net/?retryWrites=true&w=majority/bibliotrack',
     secretKey: 'secret'
 }
